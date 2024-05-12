8af7e1dca3dbd2d8fc9b939e2c76251ee84e5fbc
Temitayo <temfoden@gmail.com>
2024-05-12 05:33:25 +0100

    update advance task solution

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,21 +1,15 @@
 #!/usr/bin/env ruby
 
-# Define a method to extract sender, receiver, and flags from a log entry
 def extract_info(log_entry)
     sender = log_entry.match(/\[from:(?<sender>[^\]]+)\]/)[:sender]
     receiver = log_entry.match(/\[to:(?<receiver>[^\]]+)\]/)[:receiver]
     flags = log_entry.match(/\[flags:(?<flags>[^\]]+)\]/)[:flags]
-    
-    [sender, receiver, flags]
+    "#{sender},#{receiver},#{flags}"
   end
   
-  # Read each line of the log file and process it
-  File.open(ARGV[0], "r") do |file|
-    file.each_line do |line|
-      if line.include?("Sent SMS") || line.include?("Receive SMS")
-        sender, receiver, flags = extract_info(line)
-        puts "#{sender},#{receiver},#{flags}"
-      end
-    end
+  ARGF.each_line do |line|
+    next unless line.include?("mdr: ")
+    log_entry = line.match(/mdr:.*?\[(.*?)\]/)[1]
+    puts extract_info(log_entry)
   end
   
